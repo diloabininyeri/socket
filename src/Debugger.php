bb6a2fc7b9320d090b86af66c4c53aaeaeffd414
Dilo surucu <berxudar@gmail.com>
2024-01-05 00:12:35 -0500

    debugger outputs are colored (green)

--- src/Debugger.php
+++ src/Debugger.php
@@ -2,10 +2,12 @@
 
 namespace Zeus\Pusher;
 
+use Override;
+
 class Debugger implements DebuggerInterface
 {
 
-    #[\Override]
+    #[Override]
     public function onRead(string $message): void
     {
         $date = date('Y-m-d H:i:s');
