f32030e4058665efc04f580fd13b82022f6a44c8
Dilo surucu <berxudar@gmail.com>
2024-01-08 19:32:05 -0500

    Example usage for client added to description

--- test/client.php
+++ test/client.php
@@ -3,11 +3,12 @@
 
 use Zeus\Pusher\SocketClient;
 
-require_once '../vendor/autoload.php';
+
 
 
 $socketClient = new SocketClient('0.0.0.0', 8080);
 
-$socketClient->sleep(2);
+$socketClient->sleep(1);
 $socketClient->send('hello world');
+
 echo $socketClient->read();
